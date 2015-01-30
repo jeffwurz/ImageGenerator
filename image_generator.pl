79544459be3096168cbc62fc70557f957b6d4561
jeffwurz <jeff.wurz@gmail.com>
2015-01-30 07:23:49 -0800

    actual first good commit, last one was the wrong file, started the repo where I couldn't run the scripts easily

--- image_generator.pl
+++ image_generator.pl
@@ -1,36 +1,53 @@
 #!/usr/bin/perl -w
 use strict;
 use SVG;
-use Image::LibRSVG;
 
 # create an SVG object
 my %hash;
-$hash{width} = 1000;
-$hash{height} = 1000;
-my $filename = "filename.svg";
-my $filename = "filename.png";
+$hash{width} = 750;
+$hash{height} = 750;
+my $filename = "./filename.svg";
+my $filename2 = "./filename.png";
 my $svg= SVG->new(%hash); 
 
-# draw a circle at position (100,100) with ID 'this_circle'
-$svg->circle(id=>'this_circle',cx=>100,cy=>100,r=>50);
-open FILE, ">", $filename or die $!
+#
+my $x;
+my $xpitch = 12;
+my $y;
+my $ypitch = 11;
+my $radius= 12;
+my $color = "red";
+my $outline_color = "black";
+my $outline_width = 10;
+my $id;
+my @color_list = ("red","blue","black");
+#method for list of colors;
+#method for varying size of elements randomly with tolerance
+#
+$svg->rect( x=>0,
+            y=>0,
+            height=>$hash{height},
+            width=>$hash{width},
+            fill=>"green");
+foreach(@color_list){
+    for(my $x = 0; $x <= $hash{width}; $x=$x+$xpitch){
+        for(my $y = 0; $y <= $hash{height}; $y=$y+$ypitch){
+
+            $id = $x."_".$y."_".$radius."_".$_; 
+            #drawacircle
+            $svg->circle(id=>$id,
+                         cx=>$x,
+                         cy=>$y,
+                         r=>$radius,
+                         fill=>$_);
+            print $_."\n";
+            $xpitch+=10;
+            $ypitch+=15;
+        }
+    }
+}
+
+open(FILE, '>', $filename) or die "Could not open file '$filename' $!";
 my $out = $svg->xmlify;
 print FILE $out;
-convert($filename,filename2);
-
-#convert SVG to PNG
-sub convert
-{
-    my $in  = shift;
-    my $out = shift;
-    if (not $out) {
-    	die <<"END_USAGE";
-    Converting svg file to png file:
-    
-    Usage: $0 file.svg  file.png
-    END_USAGE
-    }
-    
-    my $rsvg = Image::LibRSVG->new();
-    $rsvg->convert( $in, $out );
-}
+`convert $filename $filename2`;
